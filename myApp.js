cc663906834a60edabdeb138a567bee4013c5bad
Shrishti <shrishti2454soam@gmail.com>
2021-05-11 18:31:03 +0530

    findOneAndUpdate

--- myApp.js
+++ myApp.js
@@ -95,8 +95,14 @@ const findEditThenSave = (personId, done) => {
 
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
+  Person.findOneAndUpdate({name:personName},{age : ageToSet},{ new: true },function(err,result){
+    if(err){
+      console.log(err);
+    } done(null ,data);
+    
+  })
 
-  done(null /*, data*/);
+ 
 };
 
 const removeById = (personId, done) => {
